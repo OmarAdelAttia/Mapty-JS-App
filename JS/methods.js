13fba4813eda9f24d77f357b33456e8eb1a6940f
Omar Adel <oadel@kn-it.com>
2023-10-08 16:26:56 +0300

    deleting files

--- JS/methods.js
+++
@@ -1,9 +0,0 @@
-import * as element from "./elements.js";
-
-// export const successCallback = position => console.log(position);
-
-// export function successCallback(params) {
-//     console.log(position);
-// }
-
-export const errorCallback = () => alert('Could not get your position');
